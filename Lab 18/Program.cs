f6204825e48581d11cf8242e0b37254b879bb564
Андрей Мисюркеев <misyurckeev.andrey@yandex.ru>
2021-10-26 01:01:45 +0300

    Добавьте файлы проекта.

---
+++ Lab 18/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            string Sample = "([]{})[]";
+            Console.WriteLine(Temlate(Sample));
+            Console.ReadKey();
+        }
+        public static bool Temlate(string str)
+        {
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in str)
+            {
+                if (c == '(' || c == '[' || c == '{')
+                {
+                    stack.Push(c);
+                    continue;
+                }
+                if (c == ')' && stack.Peek() == '(')
+                {
+                    stack.Pop();
+                }
+
+                if (c == ']' && stack.Peek() == '[')
+                {
+                    stack.Pop();
+                }
+
+                if (c == '}' && stack.Peek() == '{')
+                {
+                    stack.Pop();
+                }
+            }
+            if (stack.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
